a944baacca04e1511883b7d65c37ea19280a13b2
KimJeongYun20167 <keaton20167@gmail.com>
2026-03-13 15:15:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -92,7 +92,8 @@ if mode == "학생용 제출":
             try:
                 save_submission(title, content)
                 st.success("제출 완료!")
-                st.balloons()
+                st.snow()
+                #st.balloons()
             except requests.HTTPError as e:
                 st.error("제출 요청이 실패했어.")
                 st.exception(e)

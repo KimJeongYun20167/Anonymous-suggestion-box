7b93edc5ef724bb2065bb19d843dd624e72d1a71
KimJeongYun20167 <keaton20167@gmail.com>
2026-03-14 15:27:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.markdown("""
 /* 전체 배경 */
 .stApp {
     background:
-        linear-gradient(rgba(248,247,242,0.93), rgba(248,247,242,0.93)),
+        linear-gradient(rgba(248,247,242,0.9), rgba(248,247,242,0.9)),
         url("https://raw.githubusercontent.com/KimJeongYun20167/Anonymous-suggestion-box/main/clover_bg.jpg");
     background-size: 320px auto;
     background-repeat: repeat;
